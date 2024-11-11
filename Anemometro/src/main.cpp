ab43acd5af5000df02338fcb56027fd6147ec54e
SantiQ <a01571222@itesm.mx>
2024-11-10 22:41:48 -0600

    wind sensor done

--- Anemometro/src/main.cpp
+++ Anemometro/src/main.cpp
@@ -12,39 +12,147 @@ Irvin David Ornelas García A00839065
 */
 
 #include <Arduino.h> // Incluye la librería base de Arduino para poder utilizar sus funciones.
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 
-// Define el pin de entrada analógica.
 int windPin = A0;
-
-// Define el voltaje que emite el sensor cuando el ventilador no está girando.
 float zeroWindVoltage = 0.04;
-
-// Define el cambio de voltaje por cada unidad de velocidad del viento (mV por kph).
 float voltagePerkph = 0.1;
 
-// Función 'setup' que se ejecuta una vez al inicio cuando se enciende o reinicia el microcontrolador.
+const char* ssid = "INFINITUM8B9C_2.4";
+const char* password = "f54cBnRD7e";
+
+const int ID = 1;
+const int ID_ASensor = 4;
+const int ID_Plant = 1;
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 60000);
+
+String URL = "http://192.168.1.194:3000/iot/api/insertAnemometer";
+
+void logIntento(float windSpeedkph, String formattedDate);
+String formatDateTime(unsigned long unixTime);
+unsigned long getTime();
+
 void setup() {
-  // Inicializa la comunicación serial.
-  Serial.begin(115200); // El ESP8266 normalmente usa una tasa de baudios más alta.
+  Serial.begin(9600); 
+  Serial.println("***Inicializando conexión a My SQL***");
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando a red WiFi \"");
+  Serial.print(ssid);
+  Serial.print("\"");
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.print("\nConectado! IP: ");
+  Serial.println(WiFi.localIP());
+
+  timeClient.begin();
+
+  delay(500);
 }
 
 void loop() {
-  // Lee el voltaje del sensor de viento.
-  float voltage = analogRead(windPin) * 3.3 / 1023.0; // El ESP8266 usa una referencia de 3.3V.
-
-  // Calcula la velocidad del viento en kph.
+  float voltage = analogRead(windPin) * 3.3 / 1023.0; 
   float windSpeedkph = (voltage - zeroWindVoltage) / voltagePerkph;
 
-  // Asegura que la velocidad del viento siempre sea positiva.
+  if (isnan(windSpeedkph)) {
+    windSpeedkph = 0;
+  }
+
   if (windSpeedkph < 0) {
     windSpeedkph = 0;
   }
 
-  // Imprime la velocidad del viento en el monitor serial.
+  unsigned long unixTime = getTime();
+  String formattedDate = formatDateTime(unixTime);
+
   Serial.print("Wind speed: ");
   Serial.print(windSpeedkph);
   Serial.println(" kph");
 
-  // Espera un periodo corto antes de tomar la siguiente medición.
+  Serial.print("\nFormatted Date: ");
+  Serial.println(formattedDate);
+
+  logIntento(windSpeedkph, formattedDate);
+
   delay(1000);
+}
+
+void logIntento(float windSpeedkph, String formattedDate) {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient wClient;
+    HTTPClient httpClient;
+
+    // Crear el JSON a enviar
+    String jsonPayload = String("{") +
+                    "\"ID_ASensor\": " + String(ID_ASensor) + ", " +
+                    "\"ID_Plant\": " + String(ID_Plant) + ", " +
+                    "\"Value\": " + String(windSpeedkph) + ", " +
+                    "\"Date\": \"" + formattedDate + "\"" +
+                    "}";
+    
+    Serial.println("Payload JSON: " + jsonPayload);
+    Serial.print("Conectando a URL: ");
+    Serial.println(URL);
+
+    // Inicia la conexión
+    httpClient.begin(wClient, URL);
+    httpClient.addHeader("Content-Type", "application/json");
+
+    // Envía la solicitud POST y recibe el código de respuesta
+    int httpResponseCode = httpClient.POST(jsonPayload);
+    
+    Serial.print("Código de respuesta HTTP: ");
+    Serial.println(httpResponseCode);
+
+    if (httpResponseCode > 0) {
+      // Si la respuesta fue exitosa, imprime el contenido
+      String response = httpClient.getString();
+      Serial.print("Respuesta del servidor: ");
+      Serial.println(response);
+    } else {
+      // Si hubo un error, muestra información adicional
+      Serial.print("Error en la solicitud POST: ");
+      Serial.println(httpResponseCode);
+      Serial.println("Causas posibles: URL incorrecta, servidor inaccesible o problema de red.");
+    }
+    
+    httpClient.end(); 
+  } else {
+    Serial.println("Error: No está conectado a WiFi. Intentando reconectar...");
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.print(".");
+    }
+    Serial.println("\nReconectado a WiFi.");
+  }
+}
+
+unsigned long getTime() {
+  timeClient.update();
+  return timeClient.getEpochTime();
+}
+
+// Función para convertir Unix timestamp a formato "YYYY-MM-DD HH:MM:SS"
+String formatDateTime(unsigned long unixTime) {
+  time_t rawTime = static_cast<time_t>(unixTime);  // Convert unsigned long to time_t
+  struct tm *dt;
+  char buffer[20];
+  dt = localtime(&rawTime);
+  strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", dt);
+  return String(buffer);
 }
--- Humedad/src/main.cpp
+++ Humedad/src/main.cpp
@@ -11,21 +11,96 @@ Santiago Quintana Moreno A01571222
 */
 
 #include <Arduino.h> // Incluye la librería principal de Arduino.
+#include <ESP8266WiFi.h> // Incluye la librería de ESP8266 para conexión WiFi.
+#include <ESP8266HTTPClient.h> // Incluye la librería de ESP8266 para realizar peticiones HTTP.
 
 int sensor = A0; // Declara una variable entera llamada 'sensor' y la asigna al pin analógico A0.
 
+const char* ssid = "INFINITUM8B9C_2.4";
+const char* password = "f54cBnRD7e";
+
+String URL = "http://192.168.1.194:3000/insertSoilMoistureLog";
+
+void logIntento(int humedad);
+
 // Función 'setup' que se ejecuta una vez al inicio cuando se enciende o reinicia el microcontrolador.
 void setup() {
-  Serial.begin(115200); // Inicia la comunicación serial con una velocidad de 115200 baudios.
+  Serial.begin(9600); 
+  Serial.println("***Inicializando conexión a My SQL***");
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando a red WiFi \"");
+  Serial.print(ssid);
+  Serial.print("\"");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\nConectado! IP: ");
+  Serial.println(WiFi.localIP());
+  delay(500);
 }
 
 void loop() {
   int humedad = analogRead(sensor);  
-  // Lee el valor del sensor de humedad en el pin A0 utilizando la función 'analogRead' y almacena el resultado en la variable 'humedad'.
-  
+
+  if (isnan(humedad)) {
+    Serial.println("Error al leer el sensor de humedad!");
+    return;
+  }
+
   Serial.println(humedad); 
-  // Envía el valor de 'humedad' al puerto serial para que pueda verse en el monitor serie del IDE de Arduino.
+  
+  logIntento(humedad);
   
   delay(1000); 
-  // Pausa la ejecución del código durante 1000 milisegundos (1 segundo) antes de repetir el ciclo.
+}
+
+void logIntento(int humedad) {
+  if (WiFi.status() == WL_CONNECTED){
+    WiFiClient wClient;
+    HTTPClient httpClient;
+
+    String jsonPayload = "{\"humedad\":" + String(humedad) + "}";
+    Serial.println("Payload JSON: " + jsonPayload);
+    Serial.print("Conectando a URL: ");
+    Serial.println(URL);
+    
+    // Inicia la conexión
+    httpClient.begin(wClient, URL);
+    httpClient.addHeader("Content-Type", "application/json");
+
+    // Envía la solicitud POST y recibe el código de respuesta
+    int httpResponseCode = httpClient.POST(jsonPayload);
+    
+    Serial.print("Código de respuesta HTTP: ");
+    Serial.println(httpResponseCode);
+
+    if (httpResponseCode > 0) {
+      // Si la respuesta fue exitosa, imprime el contenido
+      String response = httpClient.getString();
+      Serial.print("Respuesta del servidor: ");
+      Serial.println(response);
+    } else {
+      // Si hubo un error, muestra información adicional
+      Serial.print("Error en la solicitud POST: ");
+      Serial.println(httpResponseCode);
+      Serial.println("Causas posibles: URL incorrecta, servidor inaccesible o problema de red.");
+    }
+    
+    httpClient.end(); 
+  } else {
+    Serial.println("Error: No está conectado a WiFi. Intentando reconectar...");
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.print(".");
+    }
+    Serial.println("\nReconectado a WiFi.");
+  }
 }
